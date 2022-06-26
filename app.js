b830c7d9a5e3a9c7f2eec75f1338ddaa5755d175
ALEX321616 <zaplev@ukr.net>
2022-06-26 21:04:06 +0300

    .

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const cors = require("cors");
 
 const contactsRouter = require("./routes/api/contacts");
 const usersRouter = require("./routes/api/users");
-// const avatarsRouter = require("./routes/api/avatars");
 const app = express();
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
@@ -14,7 +13,6 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/users", usersRouter);
 app.use("/api/contacts", contactsRouter);
-// app.use("/", avatarsRouter);
 app.use(express.static("public"));
 
 app.use((req, res) => {
